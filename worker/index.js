b698e8c68968fbabbe63115bc17804702df1aad2
Noah van der Aa <ndvdaa@gmail.com>
2021-10-27 23:08:15 +0200

    Use proper JSON for error messages

--- worker/index.js
+++ worker/index.js
@@ -8,13 +8,14 @@ const router = Router();
 
 router.get('/canvas', async () => {
     const canvas = await CANVAS.get('canvas');
+
     return new Response(canvas, { headers });
 });
 
 router.get('/canvas/:id', async ({ params }) => {
     const { id } = params;
     if (isNaN(id) || id < 0 || id > 9999) {
-        return new Response('Invalid ID', { headers });
+        return new Response(JSON.stringify({ success: false, error: 'Invalid ID' }), { headers });
     }
 
     const canvas = JSON.parse(await CANVAS.get('canvas'));
@@ -27,25 +28,25 @@ router.post('/canvas/bulk', async (request) => {
     const content = await request.json();
 
     if (typeof content !== 'object' || !content.length) {
-        return new Response('Invalid request', { headers });
+        return new Response(JSON.stringify({ success: false, error: 'Invalid request' }), { headers });
     }
 
     const canvas = JSON.parse(await CANVAS.get('canvas'));
 
     for (const action of content) {
         if (action.length !== 2) {
-            return new Response('Invalid request', { headers });
+            return new Response(JSON.stringify({ success: false, error: 'Invalid request' }), { headers });
         }
 
         const id = action[0];
         const color = action[1];
 
         if (isNaN(id) || id < 0 || id > 9999) {
-            return new Response('Invalid ID', { headers });
+            return new Response(JSON.stringify({ success: false, error: 'Invalid ID' }), { headers });
         }
 
         if (isNaN(color) || color < 0 || color > 15) {
-            return new Response('Invalid color', { headers });
+            return new Response(JSON.stringify({ success: false, error: 'Invalid color' }), { headers });
         }
 
         canvas[id] = [parseInt(color), new Date().getTime()];
@@ -53,19 +54,19 @@ router.post('/canvas/bulk', async (request) => {
 
     await CANVAS.put('canvas', JSON.stringify(canvas));
 
-    return new Response({ success: true }, { headers });
+    return new Response(JSON.stringify({ success: true }), { headers });
 });
 
 router.post('/canvas/:id', async (request) => {
     const { id } = request.params;
     if (isNaN(id) || id < 0 || id > 9999) {
-        return new Response('Invalid ID', { headers });
+        return new Response(JSON.stringify({ success: false, error: 'Invalid ID' }), { headers });
     }
 
     const content = await request.json();
     const { color } = content;
     if (isNaN(color) || color < 0 || color > 15) {
-        return new Response('Invalid color', { headers });
+        return new Response(JSON.stringify({ success: false, error: 'Invalid color' }), { headers });
     }
 
     const canvas = JSON.parse(await CANVAS.get('canvas'));
@@ -73,7 +74,7 @@ router.post('/canvas/:id', async (request) => {
 
     await CANVAS.put('canvas', JSON.stringify(canvas));
 
-    return new Response({ success: true }, { headers });
+    return new Response(JSON.stringify({ success: true }), { headers });
 });
 
 router.all('*', () => new Response('404, not found!', { status: 404 }));
